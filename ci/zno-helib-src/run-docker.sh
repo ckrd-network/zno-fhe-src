caa68d8a0599e28732e66f5e41f83bcc9c5cbee6
Mark Van de Vyver <mark@ckrd.io>
2023-10-02 14:04:48 +1100

    [Evolve][Core] Refactor
    Signed-off-by: Mark Van de Vyver <mark@ckrd.io>

---
+++ ci/zno-helib-src/run-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+target=$1
+
+if [ ! -d ci/docker/$1 ]; then
+  exec ci/run.sh $1
+fi
+
+set -ex
+
+docker build \
+  --rm \
+  --tag ci-zno-helib-src \
+  ci/zno-helib-src/docker/$1
+
+docker run \
+  --rm \
+  --volume "$(rustc --print sysroot)":/rust:ro \
+  --volume "$(pwd)":/usr/code:ro \
+  --volume "$(pwd)"/target:/usr/code/target \
+  --volume $HOME/.cargo:/cargo \
+  --env CARGO_HOME=/cargo \
+  --workdir /usr/code \
+  ci-zno-helib-src \
+  bash -c "PATH=\$PATH:/rust/bin ci/zno-helib-src/run.sh $target"

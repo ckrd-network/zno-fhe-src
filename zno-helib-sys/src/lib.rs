d4ad6010087046490b2a337591740f484489a475
Mark Van de Vyver <mark@ckrd.io>
2023-10-11 15:02:26 +1100

    [Evolve][Core] Module structure
    Signed-off-by: Mark Van de Vyver <mark@ckrd.io>

--- zno-helib-sys/src/lib.rs
+++ zno-helib-sys/src/lib.rs
@@ -1,12 +1,14 @@
 pub mod prelude;
-pub mod zno;
+mod zno;
 
 use autocxx::prelude::*; // use all the main autocxx functions
 
 include_cpp! {
     // C++ headers to include, relative to the path in build/main.rs.
     #include "helib/helib.h"
+    #include "helib/ArgMap.h"
     safety!(unsafe_ffi) // see details of unsafety policies described in the 'safety' section of the book
     // generate_ns!("helib") // add this line for each function or type you wish to generate
-    generate!("helib::Context") // add this line for each function or type you wish to generate
+    // generate!("helib::Context") // zno-helib-sys/src/autocxx/context.rs
+    generate!("helib::ArgMap") // zno-helib-sys/src/autocxx/arg_map.rs
 }
--- zno-helib-sys/tests/integration/builtin/test.rs
+++ zno-helib-sys/tests/integration/builtin/test.rs
@@ -1,3 +1,5 @@
+use zno::prelude::*;
+
 #[cfg(test)]
 mod tests {
     #[test]

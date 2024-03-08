a16cd55901f02fa927f5b32465aec69d52b84cd8
Mark Van de Vyver <mark@ckrd.io>
2024-03-08 18:30:21 +1100

    [Evolve][HELib] Linting
    Signed-off-by: Mark Van de Vyver <mark@ckrd.io>

--- zno-helib-sys/src/prelude.rs
+++ zno-helib-sys/src/prelude.rs
@@ -11,11 +11,11 @@ pub use crate::bgv::ords::*;
 pub use crate::bgv::mvec::*;
 pub use crate::bgv::bootstrap::*;
 pub use crate::bgv::bootstrappable::*;
+
 pub use crate::bgv::parameters::*;
 pub use crate::bgv::context::*;
 pub use crate::bgv::error::*;
 pub use crate::bgv::metric::*;
-
 pub use crate::bgv::He;
 pub use crate::bgv::Schema;
 pub use crate::bgv::Scheme;
